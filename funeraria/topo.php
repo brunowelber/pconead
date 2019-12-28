4280390f70fe25c3badfe787ee08a80ef4e8a1db
sidneisilvestre <sidsilvestrenei@gmail.com>
2019-12-28 17:35:14 -0200

    funeraria

---
+++ funeraria/topo.php
@@ -0,0 +1,23 @@
+﻿<?php 
+echo "Aqui ficará o logo"
+    ?>
+<html>
+<a href="#conteudo" accesskey="1">ir para o conteudo principal</a>
+<a href="#rodape" accesskey="2">ir para o Rodapé</a>
+<form>
+<label for="login">Informe seu CPF </label>
+<input type="number" name="login" id="login" required />
+<label for="pas">Informe sua senha </label>
+<input type="password" name="pas" id="pas" required />
+<button type="submit">Entrar</button>
+</form>
+<a href="esqueciasenha.html">Esqueci a Senha </a>
+<a href="cadastro.php">Cadastre-se</a>
+<form role="search">
+<label for="p">busque no site</label>
+<input type="text" id="p" name="p" />
+<button type="submit">pesquisar</button>
+</form> 
+
+
+</html>
